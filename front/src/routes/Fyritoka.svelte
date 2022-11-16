90d9c1727d40ccfdca1bae454fcf52ae6daf3c70
Hanus á Tjaldrafløtti Vestergaard <hanusatv@gmail.com>
2022-11-16 01:14:50 +0100

    routes in store

--- front/src/routes/Fyritoka.svelte
+++ front/src/routes/Fyritoka.svelte
@@ -1,14 +1,17 @@
 <script>
     import LontakariListBox from "../elements/LontakariListBox.svelte";
+    import { selectedRoute } from "/src/stores/store.js";
+    selectedRoute.set("fyritoka");
 
     let lonarfolk2 = [];
     const HeintaOllLonarFolk = (async () => {
-            const response = await fetch('http://127.0.0.1:8000/fyritoka/');
-            lonarfolk2 = await response.json();
-        return await response.json()
-	})();
-    const tableHeading = ["ID", "Navn", "Gøta","Býður", "Post Nummar", "Land"];    
+        const response = await fetch("http://127.0.0.1:8000/fyritoka/");
+        lonarfolk2 = await response.json();
+        return await response.json();
+    })();
+    const tableHeading = ["ID", "Navn", "Gøta", "Býður", "Post Nummar", "Land"];
 </script>
+
 <ul class="employee-list">
     <li class="new employee-list-item">
         <img
@@ -27,20 +30,20 @@
 
         {#await lonarfolk2}
             {#each Array(5) as _}
-            <li>
-                <LontakariListBox />
-            </li>
+                <li>
+                    <LontakariListBox />
+                </li>
             {/each}
         {:then lonarfolk2}
             {#each lonarfolk2 as eittlonarfolk}
-            <tr>
-                <th scope="row">{eittlonarfolk._id}</th>
-                <td>{eittlonarfolk.Navn}</td>
-                <td>{eittlonarfolk.Gøta}</td>
-                <td>{eittlonarfolk.Bydur}</td>
-                <td>{eittlonarfolk.postNummar}</td>
-                <td>{eittlonarfolk.Land}</td>
-            </tr>
+                <tr>
+                    <th scope="row">{eittlonarfolk._id}</th>
+                    <td>{eittlonarfolk.Navn}</td>
+                    <td>{eittlonarfolk.Gøta}</td>
+                    <td>{eittlonarfolk.Bydur}</td>
+                    <td>{eittlonarfolk.postNummar}</td>
+                    <td>{eittlonarfolk.Land}</td>
+                </tr>
             {/each}
         {:catch error}
             <p>An error occurred!</p>
