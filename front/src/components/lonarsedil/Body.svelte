7ba6f06d31407878a51787eedba175427ea9f7b8
ari <props1993@gmail.com>
2022-12-16 21:51:57 +0100

    PDF Converter

--- front/src/components/lonarsedil/Body.svelte
+++ front/src/components/lonarsedil/Body.svelte
@@ -12,27 +12,40 @@
 	})
 </script>
 
-<style>
-    address {
-        width: 50%;
+<table class="invoice">
+    <tr class="header">
+      <th class="fragreiding">Frágreiðing</th>
+      <th>Nøgd</th>
+      <th>Satsur</th>
+      <th class="ialt">Tilsamans</th>
+    </tr>
+
+    <tr class="footer footer-total">
+      <th colspan="3">í Alt:</th>
+      <th colspan="3">10000,00 DKK</th>
+      
+    </tr>
+  </table>
+
+  <style>
+    .header {
+        border-bottom: 1px solid black;
+        height: 20px;
+    }
+    .invoice {
+        width: 100%;
+        height: 50%;
         float: right;
         padding: 0 10px;
         margin-bottom: 15px;
         font-style: normal;
         text-align: right;
+        font-size: 15px;
+    }
+    .fragreiding {
+        text-align: left;
+    }
+    .ialt {
+        text-align: right;
     }
 </style>
-  
-<address>
-    {lonarfolk.Fyritoka}<br />
-    <br />
-    {lonarfolk.Navn}<br />
-    {lonarfolk.Bydur}<br />
-    {lonarfolk.Bustadur}<br />
-    {lonarfolk.Mail}<br />
-    {lonarfolk.Telefon}<br />
-    {lonarfolk.Ptal}<br />
-    {lonarfolk.Starvsheiti}<br />
-    {lonarfolk.Fødingardagur}<br />
-
-</address>
