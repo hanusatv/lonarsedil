e9979a19d0d3918a0a3b1544f32c8085c9766f99
Hanus á Tjaldrafløtti Vestergaard <hanusatv@gmail.com>
2023-03-16 02:16:00 +0100

    Metodir til løntakari klassan

--- front/src/classes/classes.js
+++ front/src/classes/classes.js
@@ -1,31 +1,47 @@
 const baseUrl = "BASE_URL_BACK"
 
 export class Lonarfolk {
-    constructor() {
-        // const yamlData = yaml.load(fs.readFileSync("../../../.env.yaml", 'utf8'));
-        // console.log(yamlData)
-    }
-
-
     static async heintaOll() {
-        const response = await fetch(baseUrl + "/lontakari");
+        const response = await fetch(`${baseUrl}/lontakari`);
         return await response.json();
     }
 
     static async heintaEin(id) {
-        const response = await fetch(baseUrl + "/lontakari/" + id);
+        const response = await fetch(`${baseUrl}/lontakari/${id}`);
         return await response.json();
     }
 
     static async dagfor(id, felt) {
-        console.log(id, felt)
-        const response = await fetch(baseUrl + "/lontakari/" + id, {
+        const response = await fetch(`${baseUrl}/lontakari/${id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
                 'accept': 'application/json'
             },
             body: JSON.stringify(felt)
+        });
+        return await response.json();
+    }
+
+    static async upraetta(felt) {
+        const response = await fetch(`${baseUrl}/lontakari/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify(felt)
+        });
+        return await response.json();
+    }
+
+    static async sletta(id) {
+        const response = await fetch(`${baseUrl}/lontakari/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            }
         });
         return await response.json();
     }
