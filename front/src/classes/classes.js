f20842a0ac4c32789cadabd3f39c77c543beaec8
Hanus á Tjaldrafløtti Vestergaard <hanusatv@gmail.com>
2023-06-07 23:46:44 +0100

    Byrja uppá fyritøkukort

--- front/src/classes/classes.js
+++ front/src/classes/classes.js
@@ -100,3 +100,92 @@ export class Lonarfolk {
         }
     }
 }
+
+class FyritokaData {
+    constructor(data) {
+        this.data = {
+            _id: "",
+            Navn: "",
+            Gøta: "",
+            Bydur: "",
+            PostNummar: "",
+            Land: "",
+            KontoNummar: "",
+            Telefon: "",
+            V_Tal: ""
+        };
+        //Iterate over the data, which is an JSON object, and replace the values in the data object with the values from the JSON object
+        for (const [key, value] of Object.entries(data)) {
+            this.data[key] = value;
+        }
+    }
+}
+
+export class Fyritoka {
+
+    static async heintaOll() {
+        try {
+            const response = await fetch(`${baseUrl}/fyritoka`);
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async heintaEina(id) {
+        try {
+            const response = await fetch(`${baseUrl}/fyritoka/${id}`);
+            //Pass the response to the LontakariData class, which will create a new object with the data from the response
+            return new FyritokaData(await response.json());
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async dagfor(id, felt) {
+        try {
+            const response = await fetch(`${baseUrl}/fyritoka/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept': 'application/json'
+                },
+                body: JSON.stringify(felt)
+            });
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async upraetta(felt) {
+        try {
+            const response = await fetch(`${baseUrl}/fyritoka/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept': 'application/json'
+                },
+                body: JSON.stringify(felt)
+            });
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async sletta(id) {
+        try {
+            const response = await fetch(`${baseUrl}/fyritoka/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept': 'application/json'
+                }
+            });
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
--- front/src/routes/Fyritoka.svelte
+++ front/src/routes/Fyritoka.svelte
@@ -1,65 +1,77 @@
 <script>
     import LontakariListBox from "../elements/LontakariListBox.svelte";
     import { selectedRoute } from "../stores/store.js";
+    import { Fyritoka } from "../classes/classes";
+
     selectedRoute.set("fyritoka");
 
-    let lonarfolk2 = [];
-    const HeintaOllLonarFolk = (async () => {
-        const response = await fetch("http://127.0.0.1:8000/fyritoka/");
-        lonarfolk2 = await response.json();
-        return await response.json();
-    })();
-    const tableHeading = ["ID", "Navn", "Gøta", "Býður", "Post Nummar", "Land"];
+    const fyritokur = Fyritoka.heintaOll();
+
+    //Write a function that calls Lonarfole.upaetta() and the redirect to the new employee page (lontakari.svelte) with the ID returned from the function
+    function upraetta() {
+        Fyritoka.upraetta()
+            .then((res) => {
+                window.location.href = `http://localhost:5173/#/fyritoka/${res._id}`;
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    }
 </script>
 
-<ul class="employee-list">
-    <li class="new employee-list-item">
-        <img
-            class="svg-icon svg-color"
-            src="/src/assets/icons/new-pluss.svg"
-            alt="New"
-        />
-    </li>
-    <!-- koda her -->
-    <div>
-        <tr>
-            {#each tableHeading as heading}
-                <th>{heading}</th>
-            {/each}
-        </tr>
+<button class="new" on:click={upraetta}>
+    <img
+        class="svg-icon svg-color"
+        src="/src/assets/icons/company.svg"
+        alt="New"
+    />
+</button>
 
-        {#await lonarfolk2}
+<div class="company-list-container">
+    <ul class="company-list">
+        {#await fyritokur}
             {#each Array(5) as _}
                 <li>
                     <LontakariListBox />
                 </li>
             {/each}
-        {:then lonarfolk2}
-            {#each lonarfolk2 as eittlonarfolk}
-                <tr>
-                    <th scope="row">{eittlonarfolk._id}</th>
-                    <td>{eittlonarfolk.Navn}</td>
-                    <td>{eittlonarfolk.Gøta}</td>
-                    <td>{eittlonarfolk.Bydur}</td>
-                    <td>{eittlonarfolk.postNummar}</td>
-                    <td>{eittlonarfolk.Land}</td>
-                </tr>
+        {:then fyritokur}
+            {#each fyritokur as fyritoka}
+                <div class="company-row" id={fyritoka._id}>
+                    <span class="company-name">
+                        <a
+                            href="http://localhost:5173/#/fyritoka/{fyritoka._id}"
+                        >
+                            {fyritoka.Navn}
+                        </a>
+                    </span>
+                    <button class="company-button">
+                        <a
+                            href="http://localhost:5173/#/fyritoka/{fyritoka._id}"
+                        >
+                            <img
+                                class="svg-icon svg-color"
+                                src="/src/assets/icons/create.svg"
+                                alt="edit"
+                            />
+                        </a>
+                    </button>
+                </div>
             {/each}
         {:catch error}
-            <p>An error occurred!</p>
+            <p>{error.message}</p>
         {/await}
-    </div>
-</ul>
+    </ul>
+</div>
 
 <style>
-    .employee-list {
-        display: flex;
-        flex-direction: column;
-    }
-
     .new {
         background-color: var(--green-color);
         transition: background-color 0.2s;
+        height: 3em;
+        width: 100%;
+        border-radius: 1em;
+        display: flex;
     }
 
     .new:hover {
@@ -67,11 +79,29 @@
         background-color: var(--hover-green-color);
     }
 
-    .employee-list-item {
-        height: 3em;
-        width: 100%;
-        border-radius: 1em;
+    .company-list-container {
+        margin-top: 2em;
+    }
+
+    .company-list {
         display: flex;
+        flex-direction: column;
+    }
+
+    .company-row {
+        display: flex;
+        height: 3em;
+        border-bottom: 1px solid var(--lines-color);
+    }
+
+    .company-name {
+        flex-grow: 1;
+        align-self: center;
+        text-align: left;
+    }
+
+    .company-button {
+        background-color: transparent;
     }
 
     ul {
